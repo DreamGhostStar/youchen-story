fdf0804095161f8746fa279290ced97420a9bcfd
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-11-03 09:59:23 +0000

    Deploy dreamYouchen/youchen-story to github.com/dreamYouchen/youchen-story.git:gh-pages

---
+++ precache-manifest.82d4487234b7d6ce30c816a7ef91f29c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c32916931771b1262273dce1c25f5d22",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.431367836649782abd2c.bundle.js"
+  },
+  {
+    "url": "runtime~main.431367836649782abd2c.bundle.js"
+  },
+  {
+    "revision": "c53a2386e3bc208ea620",
+    "url": "static/css/main.c1b4e371.chunk.css"
+  },
+  {
+    "url": "vendors~main.431367836649782abd2c.bundle.js"
+  },
+  {
+    "url": "vendors~main.431367836649782abd2c.bundle.js.LICENSE.txt"
+  }
+]);
